d945bf1557b087a4a5c59a276fbaa0d49da9e900
vincentcraigolsen <vincentcraigolsen2@gmail.com>
2016-01-25 22:28:42 -0700

    refactor, broke out controlElevator methods

--- src/main/java/com/vincentcraigolsen/elevatorControlSystem/ElevatorController.java
+++ src/main/java/com/vincentcraigolsen/elevatorControlSystem/ElevatorController.java
@@ -8,42 +8,57 @@ import java.util.Map;
 
 public class ElevatorController {
 
-    static Building building;
-    static ElevatorBank elevatorBank;
+    public static Building building;
+    public static ElevatorBank elevatorBank;
 
     public ElevatorController(Building building, ElevatorBank elevatorBank) {
         this.building = building;
         this.elevatorBank = elevatorBank;
     }
-    
-    
-    public static void controlElevator() { //Building building, ElevatorBank elevatorBank
-        Boolean run = true;
-        Integer topFloor = building.topFloor();
+
+    public static void controlElevator() {
+        Boolean run = true;        
 
         while (run) {
-            for (Object floor : building) {
-                Integer floorNum = ((Floor) floor).floorNumber;
-                Boolean up = ((Floor) floor).getUp();
-                Boolean down = ((Floor) floor).getDown();
+            answerBuildingElevatorRequests();
+            answerElevatorButtonPanelFloorRequests();
+            moveElevators();
+        }
+    }
 
-                if (up) {
-                    sendElevator(selectClosestElevator(floorNum, true, false), floorNum);
-                }
+    private static void answerBuildingElevatorRequests() {
+        for (Object floor : building) {
+            Integer floorNum = ((Floor) floor).floorNumber;
+            Boolean up = ((Floor) floor).getUp();
+            Boolean down = ((Floor) floor).getDown();
 
-                if (down) {
-                    sendElevator(selectClosestElevator(floorNum, false, true), floorNum);
-                }
+            if (up) {
+                sendElevator(selectClosestElevator(floorNum, true, false), floorNum);
             }
 
-            for (Object elevator : elevatorBank) {
-                for (Map.Entry<Integer, Boolean> buttonEntry : ((Elevator) elevator).getButtonPanel().getFloorButtons().entrySet()) {
-                    if (buttonEntry.getValue() == true) {
+            if (down) {
+                sendElevator(selectClosestElevator(floorNum, false, true), floorNum);
+            }
+        }
+    }
+
+    private static void answerElevatorButtonPanelFloorRequests() {
+        for (Object elevator : elevatorBank) {
+            for (Map.Entry<Integer, Boolean> buttonEntry : ((Elevator) elevator).getButtonPanel().getFloorButtons().entrySet()) {
+                if (buttonEntry.getValue() == true) {
 //                        sendElevator((Elevator) elevator,)
-                    }
                 }
             }
         }
+    }
+
+    private static void moveElevators() {
+        Integer topFloor = building.topFloor();
+        
+        //close the door if necessary
+        //change the direction of the elevator if necessary
+        //increment floorsTraveled by absolute value of currentFloor-destinationFloor
+        //increment tripsMade by 1
     }
 
     private static Elevator selectClosestElevator(Integer floorNumCalled, Boolean up, Boolean down) {
@@ -53,15 +68,14 @@ public class ElevatorController {
         //else if elevators are heading in that direction and will pass the floor, send the closest one
         //else if there are stationary elevators, send the closest stationary elevator
         //else send the next elevator that changes direction
+
         return elevator;
     }
 
     private static void sendElevator(Elevator elevator, Integer destinationFloor) {
         //send the elevator to the floor specified
-        //change the destinationFloor of the elevator
-        //close the door if necessary
-        //change the direction of the elevator if necessary
-        //increment floorsTraveled by absolute value of currentFloor-destinationFloor
-        //increment tripsMade by 1
+        //toggle the boolean value for the buttonPanel to true for the floor requested
+        //if the floor requested is between the current position and the destinationFloor, set the destinationFloor to the requested floor
     }
+
 }
